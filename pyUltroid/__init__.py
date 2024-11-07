3a3a70f7f1f861dc50c6a1588769f38bf68682c9
hiro-v1 <mamanteng13@gmail.com>
2024-11-07 18:13:43 +0700

    Update __init__.py

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -99,7 +99,7 @@ if run_as_module:
     DUAL_HNDLR = udB.get_key("DUAL_HNDLR") or "/"
     SUDO_HNDLR = udB.get_key("SUDO_HNDLR") or HNDLR
 else:
-    print("pyUltroid 2022 © TeamUltroid")
+    print("pyhiro 2024 © hiro_v1")
 
     from logging import getLogger
 
